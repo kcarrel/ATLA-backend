0f537c9d4ce6b4bbbfc53b157d2fdc93f33384dc
Katie Carrel <katiecarrel@comcast.net>
2019-07-06 11:08:36 -0700

    first tweet sent

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,12 @@
+require 'twitter'
 class Tweet < ApplicationRecord
+
+  def self.tweet(message)
+    CLIENT.update(message)
+  end
+
+  def self.search_words(words)
+    CLIENT.search(words, lang: "en").first.text
+  end
+
 end
--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,3 +1,4 @@
+require 'twitter'
 CLIENT = Twitter::REST::Client.new do |config|
   config.consumer_key = ENV['consumer_key']
   config.consumer_secret = ENV['consumer_secret']
