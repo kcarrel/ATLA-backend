19dc63db15d32e11ecfa3aaef4d304f2f61e5161
Katie Carrel <katiecarrel@comcast.net>
2019-07-06 11:30:00 -0700

    add routes

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,5 @@
 class TweetsController < ApplicationController
+  def tweet
+    
+  end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,8 +5,4 @@ class Tweet < ApplicationRecord
     CLIENT.update(message)
   end
 
-  def self.search_words(words)
-    CLIENT.search(words, lang: "en").first.text
-  end
-
 end
